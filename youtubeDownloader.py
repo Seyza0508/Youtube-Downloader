438caa059ac225023385661843a0f4196010aa76
Seyza0508 <oluwashay.ogunleye@gmail.com>
2024-01-06 13:58:22 -0500

    took away example youtube vid that was there

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 
 # The URL of the YOutube video you want to downlaod
-videoUrl = 'https://www.youtube.com/watch?v=xt34vHxir9o'
+videoUrl = ''
 
 # Create a Youtube object with the URL
 youtube = YouTube(videoUrl)
